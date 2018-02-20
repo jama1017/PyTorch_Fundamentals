53fb134e9d892cb846e433cfc8f01dba489b5cb3
jama1017 <jiaju_ma@brown.edu>
2018-02-19 21:09:53 -0800

    Model Class created and instantiated

--- linear_regression.py
+++ linear_regression.py
@@ -5,6 +5,10 @@ import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
 
+'''
+Test Dataset
+'''
+
 x_values = [i for i in range(11)]
 x_train = np.array(x_values, dtype=np.float32)
 x_train = x_train.reshape(-1, 1)
@@ -12,3 +16,25 @@ x_train = x_train.reshape(-1, 1)
 y_values = [2*i + 1 for i in x_values]
 y_train = np.array(y_values, dtype=np.float32)
 y_train = y_train.reshape(-1,1)
+
+
+'''
+Model Class
+'''
+
+class LinearRegressionModel(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LinearRegressionModel, self).__init__()
+        self.linear = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+'''
+INSTANTIATE MODEL CLASS
+'''
+input_dim = 1
+output_dim = 1
+
+model = LinearRegressionModel(input_dim, output_dim)
