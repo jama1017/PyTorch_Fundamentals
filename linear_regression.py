26966c3e52054b8190115901b416182c5a5fc804
jama1017 <jiaju_ma@brown.edu>
2018-03-01 14:30:56 -0800

    Linear Regression Model setup complete. Now starts training

--- linear_regression.py
+++ linear_regression.py
@@ -38,3 +38,46 @@ input_dim = 1
 output_dim = 1
 
 model = LinearRegressionModel(input_dim, output_dim)
+
+'''
+INSTANTIATE LOSS CLASS
+'''
+
+criterion = nn.MSELoss()
+
+'''
+INSTANTIATE OPTIMIZER CLASS
+'''
+
+learning_rate = 0.01
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+'''
+TRAIN THE MODEL
+'''
+epochs = 100
+for epoch in range(epochs):
+    epoch += 1
+    # Convert numpy array to torch Variable
+
+        inputs = Variable(torch.from_numpy(x_train))
+        labels = Variable(torch.from_numpy(y_train))
+
+    # Clear gradients w.r.t. parameters
+    optimizer.zero_grad()
+
+    # Forward to get output
+    outputs = model(inputs)
+
+    # Calculate Loss
+    loss = criterion(outputs, labels)
+
+    # Getting gradients w.r.t. parameters
+    loss.backward()
+
+    # Updating parameters
+    optimizer.step()
+
+    # Logging
+    print('epoch {}, loss {}'.format(epoch, loss.data[0]))
