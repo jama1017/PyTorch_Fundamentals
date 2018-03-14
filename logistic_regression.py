4f78a07839bdc2b6c6759f2227af45365e8428ec
jama1017 <jiaju_ma@brown.edu>
2018-03-13 20:14:37 -0700

    created Model class

--- logistic_regression.py
+++ logistic_regression.py
@@ -37,5 +37,14 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
                                           shuffle=False)
 
+'''
+STEP 3: Create Model Class
+'''
+class LogisticRegressionModel(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LogisticRegressionModel, self).__init__()
+        self.linear = nn.Linear(input_dim, output_dim)
 
-                                         
+    def forward(self, x):
+        out = self.linear(x)
+        return out
