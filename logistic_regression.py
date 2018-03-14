effb08288d19c71abd65c6471718458b740378dd
jama1017 <jiaju_ma@brown.edu>
2018-03-13 20:11:59 -0700

    MNIST Dataset downloaded through script

---
+++ logistic_regression.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+from torch.autograd import Variable
+
+train_dataset = dsets.MNIST(root='./data',
+                            train=True,
+                            transform=transforms.ToTensor(),
+                            download=True)
+
+test_dataset = dsets.MNIST(root='./data',
+                           train=False,
+                           transform=transforms.ToTensor())
