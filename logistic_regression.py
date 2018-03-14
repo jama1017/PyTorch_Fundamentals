9eaf8b3ec352dda5272e59ba630ccb79cb117976
jama1017 <jiaju_ma@brown.edu>
2018-03-13 20:20:44 -0700

    model training code added. Ready to train the model

--- logistic_regression.py
+++ logistic_regression.py
@@ -68,3 +68,56 @@ STEP 6: Instantiate Optimizer class
 learning_rate = 0.001
 
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+'''
+STEP 7: Train the Model
+'''
+iter = 0
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # Load images as Variable
+        images = Variable(images.view(-1, 28*28))
+        labels = Variable(labels)
+
+        # Clear gradients w.r.t. parameters
+        optimizer.zero_grad()
+
+        # Forward pass to get output/logits
+        outputs = model(images)
+
+        # Calculate Loss: softmax --> cross entropy loss
+        loss = criterion(outputs, labels)
+
+        # Getting gradients w.r.t. parameters
+        loss.backward()
+
+        # Updating parameters
+        optimizer.step()
+
+        iter += 1
+
+        if iter % 500 == 0:
+            # Calculate Accuracy
+            correct = 0
+            total = 0
+            # Iterate through test dataset
+            for images, labels in test_loader:
+                #Load images to a Torch Variable
+                images = Variable(images.view(-1, 28*28)
+
+                # Forward pass only to get logits/output
+                outputs = model(images)
+
+                # Get predictions from the maximum value
+                _, predicted = torch.max(outputs.data, 1)
+
+                # Total number of labels
+                total += labels.size(0)
+
+                # Total correct predictions
+                correct += (predicted == labels).sum()
+
+            accuracy = 100 * correct / total
+
+            # Print Loss
+            print('Iteration: {}. Loss: {}. Accuracy: {}'.format(iter, loss.data[0], accuracy))
